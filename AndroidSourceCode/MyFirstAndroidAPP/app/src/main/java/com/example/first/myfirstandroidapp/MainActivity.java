5183d13a00b98744e5a27559bdcb1e2a27d0603b
王金鑫 <MrDangerous2010@gmail.com>
2015-04-05 12:16:08 +0800

    Android Based 2

--- AndroidSourceCode/MyFirstAndroidAPP/app/src/main/java/com/example/first/myfirstandroidapp/MainActivity.java
+++ AndroidSourceCode/MyFirstAndroidAPP/app/src/main/java/com/example/first/myfirstandroidapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.first.myfirstandroidapp;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -12,6 +13,7 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+Log.v("mylog","vlog");
     }
 
 
